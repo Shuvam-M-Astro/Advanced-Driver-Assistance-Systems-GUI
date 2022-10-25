46c2e9f4ede0ccc28699c1a418481f94fbc5cb28
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2022-10-25 16:56:55 +0200

    Update ADAS.py

--- ADAS.py
+++ ADAS.py
@@ -290,7 +290,50 @@ plt.show()
 
 """
 
-"""
+classes = { 0:'Speed limit (20km/h)',
+            1:'Speed limit (30km/h)', 
+            2:'Speed limit (50km/h)', 
+            3:'Speed limit (60km/h)', 
+            4:'Speed limit (70km/h)', 
+            5:'Speed limit (80km/h)', 
+            6:'End of speed limit (80km/h)', 
+            7:'Speed limit (100km/h)', 
+            8:'Speed limit (120km/h)', 
+            9:'No passing', 
+            10:'No passing veh over 3.5 tons', 
+            11:'Right-of-way at intersection', 
+            12:'Priority road', 
+            13:'Yield', 
+            14:'Stop', 
+            15:'No vehicles', 
+            16:'Veh > 3.5 tons prohibited', 
+            17:'No entry', 
+            18:'General caution', 
+            19:'Dangerous curve left', 
+            20:'Dangerous curve right', 
+            21:'Double curve', 
+            22:'Bumpy road', 
+            23:'Slippery road', 
+            24:'Road narrows on the right', 
+            25:'Road work', 
+            26:'Traffic signals', 
+            27:'Pedestrians', 
+            28:'Children crossing', 
+            29:'Bicycles crossing', 
+            30:'Beware of ice/snow',
+            31:'Wild animals crossing', 
+            32:'End speed + passing limits', 
+            33:'Turn right ahead', 
+            34:'Turn left ahead', 
+            35:'Ahead only', 
+            36:'Go straight or right', 
+            37:'Go straight or left', 
+            38:'Keep right', 
+            39:'Keep left', 
+            40:'Roundabout mandatory', 
+            41:'End of no passing', 
+            42:'End no passing veh > 3.5 tons' }
+
 model_path = "model.h5"
 loaded_model = tf.keras.models.load_model(model_path)
 
@@ -307,8 +350,8 @@ input_data = input_data/255
 
 pred = loaded_model.predict(input_data)
 result = pred.argmax()
-print(result)
-"""
+print(classes[result])
+
 ########################################################
 ########################################################
 ######### Traffic Light Detector #######################
@@ -319,7 +362,7 @@ print(result)
 import cv2
 import numpy as np
 
-
+"""
 def detect(filepath, file):
 
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -436,12 +479,12 @@ def detect(filepath, file):
 
 path = os.path.abspath('..')+'//light//'
 for f in os.listdir(path):
-    #print (f)
+    print (f)
     if f.endswith('.jpg') or f.endswith('.JPG') or f.endswith('.png') or f.endswith('.PNG'):
-        detect(path, f)
-        detect(path, f)
+        detect(path, f)    
+    print("")
             
-  
+"""  
 ########################################################
 ########################################################
 ################# Lane Detection #######################
